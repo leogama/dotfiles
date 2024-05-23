a12cf305ad367a6b6a9846cfaa84ab137c8c6506
Leonardo Gama <leonardo.gama@usp.br>
2024-05-23 15:17:12 -0300

    fixes

--- home/dot_config/sh/rc.sh
+++ home/dot_config/sh/rc.sh
@@ -1,18 +1,17 @@
 # ~/.config/sh/rc.sh: sourced by interactive shells.
 
 # Load utility functions.
-if [ -r "$XDG_USER_LIB/utils.sh" ]; then
-    . "$XDG_USER_LIB/utils.sh"
-fi
+. "$XDG_USER_LIB/utils.sh"
 
 # Set CDPATH (it's better not to export).
 CDPATH=".:$HOME:$HOME/Repos"
 
 # Load aliases and alias functions.
 try_source "$XDG_CONFIG_HOME/alias.sh"
-for file in "$XDG_CONFIG_HOME/alias.d"/*.sh; do
-    try_source "$file"
-done
-
+if [ -d "$XDG_CONFIG_HOME/alias.d" ]; then
+    for file in "$XDG_CONFIG_HOME/alias.d"/*.sh; do
+        try_source "$file"
+    done
+fi
 
 try_source "$XDG_CONFIG_HOME/sh/rc.local.sh"
