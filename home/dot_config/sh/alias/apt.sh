fe7604bc8f91d0d9693408d8f683d5586d4108da
Leonardo Gama <leonardo.gama@usp.br>
2026-04-07 21:52:59 -0300

    apt-contents: list (interesting) contents of an installed package

--- home/dot_config/sh/alias/apt.sh
+++ home/dot_config/sh/alias/apt.sh
@@ -1,1 +1,17 @@
+# vi:set ft=bash :
+
 alias apt-refresh='sudo rm -fr /var/lib/apt/lists/* && sudo apt update'
+
+function apt-contents() {
+    dpkg -L "$1" |
+        \grep -E -v \
+            -e '/(bug|icons|lib|lintian|locale)/' \
+            -e '/man/[a-z][a-z](_[A-Z][A-Z])?/' |
+        LC_COLLATE=C \sort |
+        while read filename; do
+            if [[ ! -d "$filename" ]]; then
+                echo "$filename"
+            fi
+        done |
+        more -e
+}
